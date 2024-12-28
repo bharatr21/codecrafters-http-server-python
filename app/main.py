2bdad37ee83cd5f0a66e2442cc0b358848c30cf1
Bharat Raghunathan <bharatraghunthan9767@gmail.com>
2024-12-28 14:08:48 +0530

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -9,9 +9,20 @@ def main():
     # Uncomment this to pass the first stage
     #
     server_socket = socket.create_server(("localhost", 4221), reuse_port=True)
-    client, addr = server_socket.accept() # wait for client
-    client.sendall(b'HTTP/1.1 404 Not Found\r\n\r\n')
-    client, addr = server_socket.accept() # wait for client
-    client.sendall(b'HTTP/1.1 200 OK\r\n\r\n')
+    client, client_address = server_socket.accept() # wait for client
+    while True:
+        data = client.recv(1024)
+        if not data:
+            break
+        data = data.decode().split('\r\n')
+        data = [item for item in data if item.strip()]
+        print(f"Request data: {data}")
+        url_item = data[0].split(' ')
+        method, path, protocol = url_item
+        if path == '/':
+            client.sendall(b'HTTP/1.1 200 OK\r\n\r\n')
+        else:
+            client.sendall(b'HTTP/1.1 404 Not Found\r\n\r\n')
+    client.close()
 if __name__ == "__main__":
     main()
